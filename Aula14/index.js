50564309bfda8acfb7dc2f15ae3a94c65f7db2de
Monvar Adelino Lopes <monvar.lopes@unigranrio.br>
2022-06-22 19:33:59 -0300

    Aula 14 numbers

---
+++ Aula14/index.js
@@ -0,0 +1,23 @@
+// IEEE 754-2008
+let num1 = 0.7;
+let num2 = .1;
+let num3 = 2;
+
+let result;
+/**
+let result = num1.toString() + num2;
+result = Number(result);
+num3 = Number(num3);
+result += num3;
+*/
+//toString(2)
+//toFixed(2)
+//isInteger(num1)
+//console.log(Number.isNaN(temp));
+
+result = num1 * num2 + num3;
+
+//console.log(result, typeof result);
+
+let temp = num1 + num2;
+console.log(temp);
